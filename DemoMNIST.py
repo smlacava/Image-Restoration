634f30b601728bfdf926ccc85c50fc25ec67989f
Marcorru <62520614+Marcorru@users.noreply.github.com>
2020-07-23 18:13:06 +0200

    Test with different dataset + ROC

--- DemoMNIST.py
+++ DemoMNIST.py
@@ -7,7 +7,7 @@
 from keras.datasets import mnist
 import numpy as np
 import matplotlib.pyplot as plt
-from image_denoiser import image_denoiser
+#from image_denoiser import image_denoiser
 
 (x_train, _), (x_test, _) = mnist.load_data()
 
@@ -58,7 +58,7 @@ ID.fit(x_train_noisy, x_train, 0.2, epochs=200, batch_size=128)
 # In particular, the first 10 denoised images are visually compared with the corresponding noised and original ones.
 # If the result is satisfactory, it is possible to export the resulting weights through the corresponding export_weights
 # method (in order to import them, it is possible to use the import_weights method).
-denoised = ID.predict(x_test_noisy, x_test, 10)
+[denoised,_] = ID.predict(x_test_noisy, x_test, 10,return_loss = True)
 
 # It is possible show one of the results singularly, in order to have a better view
 ax = plt.subplot(1, 2, 1)
@@ -73,3 +73,62 @@ ax.get_xaxis().set_visible(False)
 ax.get_yaxis().set_visible(False)
 plt.show()
 ID.export_weights()
+
+#Evaluation of the performance of the denoiser if it's fed with a differente dataset ( in this case with the not-mnist dataset)
+import gzip
+import sklearn.metrics as sk
+from sklearn.metrics import roc_curve, auc
+
+def extract_data(filename, num_images):
+    with gzip.open(filename) as bytestream:
+        bytestream.read(16)
+        buf = bytestream.read(28 * 28 * num_images)
+        data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+        data = data.reshape(num_images, 28,28)
+        return data
+    
+x_test_not = extract_data('notMNIST-to-MNIST-master/t10k-images-idx3-ubyte.gz', 10000)  #caricamento not-mnist test
+x_test_noisy_not = x_test_not + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test_not.shape) #noisy not mnist 
+
+x_test_noisy_not = np.clip(x_test_noisy_not, 0., 255.)
+
+[decoded_imgs_not, reconstruction_loss_not] = ID.predict(x_test_noisy_not, x_test_not, 10,return_loss = True) #not mnist tested on the autoencoder trained on mnist
+
+print('Example of denoising for the mnist')
+
+ax = plt.subplot(1, 2, 1)
+plt.imshow(x_test_noisy_not[0].reshape(28, 28))
+plt.gray()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+ax = plt.subplot(1, 2, 2)
+plt.imshow(decoded_imgs_not[0].reshape(28, 28))
+plt.gray()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+plt.show()
+
+
+ID.export_weights()
+T=np.zeros((10000,1)) #creation of vector 1 relating to the test belonging to the mnist
+F=np.ones((10000,1)) #creation of vector 0 relating to the test belonging to the not-mnist
+true=np.concatenate((T,F),axis=0)
+pred=np.concatenate((np.reshape(reconstruction_loss,(10000,1)),np.reshape(reconstruction_loss_not,(10000,1))),axis=0)
+pred=(pred-np.min(pred))/(np.max(pred)-np.min(pred)) #normalization of the scores
+
+# Compute ROC curve and ROC area
+fpr, tpr, _ = roc_curve(true,pred) 
+roc_auc = auc(fpr, tpr)
+
+plt.figure()
+
+plt.plot(fpr, tpr, color='darkorange', label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
+plt.xlim([-0.01, 1.0])
+plt.ylim([0.0, 1.01])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
+
