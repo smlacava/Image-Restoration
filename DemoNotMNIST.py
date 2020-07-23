54916241f35b859fc3f372bb2a141db46f9f9001
Marcorru <62520614+Marcorru@users.noreply.github.com>
2020-07-23 18:16:03 +0200

    Test with different dataset + ROC

--- DemoNotMNIST.py
+++ DemoNotMNIST.py
@@ -7,6 +7,7 @@ import gzip
 from image_denoiser import image_denoiser
 
 
+
 def extract_data(filename, num_images):
     with gzip.open(filename) as bytestream:
         bytestream.read(16)
@@ -23,7 +24,7 @@ x_test = extract_data('notMNIST-to-MNIST-master/t10k-images-idx3-ubyte.gz', 1000
 # and the test set.
 # In particular, the Gaussian noise distribution has zero-mean (loc) and unitary standard deviation (scale), and the
 # resulting values are multiplied by 32.
-noise_factor = 32
+noise_factor = 64
 x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
 x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
 
@@ -58,7 +59,8 @@ ID.set_name(filename)    # Assignment of the chosen name
 # Note that, even if the number of epochs is 200, the algorithm will automatically stop the training if the CNN does not
 # improve (reduce) the value of the validation loss after 3 epochs, and however only the weights with the minimum
 # validation loss value are saved at the end of the process.
-ID.fit(x_train_noisy, x_train, 0.2, epochs=300, batch_size=128)
+ID.fit(x_train_noisy, x_train, 0.2, epochs=200, batch_size=128)
+
 
 """Test of the denoiser"""
 # Finally, it is possible to predict (denoise) the images belonging to the test set and check if the results are
@@ -66,7 +68,7 @@ ID.fit(x_train_noisy, x_train, 0.2, epochs=300, batch_size=128)
 # In particular, the first 10 denoised images are visually compared with the corresponding noised and original ones.
 # If the result is satisfactory, it is possible to export the resulting weights through the corresponding export_weights
 # method (in order to import them, it is possible to use the import_weights method).
-denoised = ID.predict(x_test_noisy, x_test, 10)
+denoised = ID.predict(x_test_noisy, x_test, 10,return_loss = False)
 
 # It is possible show one of the results singularly, in order to have a better view
 ax = plt.subplot(1, 2, 1)
@@ -81,3 +83,73 @@ ax.get_xaxis().set_visible(False)
 ax.get_yaxis().set_visible(False)
 plt.show()
 ID.export_weights()
+
+#Evaluation of the performance of the denoiser if it's fed with a differente dataset ( in this case with the mnist dataset)
+
+
+import sklearn.metrics as sk
+from sklearn.metrics import roc_curve, auc
+from keras.datasets import mnist
+
+(_, _), (x_test_mnist, _) = mnist.load_data()
+
+noise_factor = 64
+x_test_noisy_mnist = x_test_mnist + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test_mnist.shape)
+
+x_test_noisy_mnist = np.clip(x_test_noisy_mnist, 0., 255.)
+[denoised_mnist, reconstruction_loss_mnist] = ID.predict(x_test_noisy_mnist, x_test_mnist, 10,return_loss = True)  #mnist tested on the autoencoder trained on not-mnist
+
+print('Example of denoising for the mnist')
+
+ax = plt.subplot(1, 2, 1)
+plt.imshow(x_test_noisy_mnist[0].reshape(28, 28))
+plt.gray()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+ax = plt.subplot(1, 2, 2)
+plt.imshow(denoised_mnist[0].reshape(28, 28))
+plt.gray()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
+plt.show()
+ID.export_weights()
+
+[decoded_imgs, reconstruction_loss] = ID.predict(x_test_noisy, x_test, 10,return_loss = True)
+print('Example of denoising for the mnist')
+
+ax = plt.subplot(1, 2, 1)
+plt.imshow(x_test_noisy[0].reshape(28, 28))
+plt.gray()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+ax = plt.subplot(1, 2, 2)
+plt.imshow(decoded_imgs[0].reshape(28, 28))
+plt.gray()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+plt.show()
+
+ID.export_weights()
+T=np.zeros((10000,1)) #creation of vector 1 relating to the test belonging to the mnist
+F=np.ones((10000,1)) #creation of vector 0 relating to the test belonging to the not-mnist
+true=np.concatenate((T,F),axis=0)
+
+pred=np.concatenate((np.reshape(reconstruction_loss,(10000,1)),np.reshape(reconstruction_loss_mnist,(10000,1))),axis=0)
+pred=(pred-np.min(pred))/(np.max(pred)-np.min(pred))
+
+# Compute ROC curve and ROC area
+fpr, tpr, _ = roc_curve(true,pred) 
+roc_auc = auc(fpr, tpr)
+
+plt.figure()
+
+plt.plot(fpr, tpr, color='darkorange', label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
+plt.xlim([-0.01, 1.0])
+plt.ylim([0.0, 1.01])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
